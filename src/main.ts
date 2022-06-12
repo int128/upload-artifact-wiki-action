cc38c5d12ec0dd7252406a05fa5ee1cac65ad50e
Hidetake Iwata <int128@gmail.com>
2022-06-12 21:44:15 +0900

    Fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { run } from './run'
 const main = async (): Promise<void> => {
   await run({
     path: core.getInput('path', { required: true }),
-    wikiUrl: core.getInput('wiki-url', { required: true }),
-    wikiBasePath: core.getInput('wiki-base-path', { required: true }),
     token: core.getInput('token', { required: true }),
   })
 }
--- src/run.ts
+++ src/run.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import * as io from '@actions/io'
+import * as github from '@actions/github'
 import * as glob from '@actions/glob'
 import { promises as fs } from 'fs'
 import * as path from 'path'
@@ -8,8 +9,6 @@ import * as git from './git'
 
 type Inputs = {
   path: string
-  wikiUrl: string
-  wikiBasePath: string
   token: string
 }
 
@@ -18,23 +17,36 @@ export const run = async (inputs: Inputs): Promise<void> => {
   const files = await globber.glob()
   core.info(`uploading ${files.length} artifact(s)`)
 
+  const wikiRepository = `${github.context.serverUrl}/${github.context.repo.owner}/${github.context.repo.repo}.wiki.git`
   const workspace = await fs.mkdtemp(path.join(os.tmpdir(), 'upload-artifact-wiki-'))
-  core.info(`cloning wiki into ${workspace}`)
-  await git.clone(workspace, inputs.wikiUrl, inputs.token)
+  core.info(`cloning ${wikiRepository} into ${workspace}`)
+  await git.clone(workspace, wikiRepository, inputs.token)
 
-  const wikiBasePath = path.join(workspace, inputs.wikiBasePath)
-  core.info(`copying artifact(s) to ${wikiBasePath}`)
-  await io.mkdirP(wikiBasePath)
-  for (const f of files) {
-    await io.cp(f, wikiBasePath)
-  }
+  const destination = path.join(workspace, getBaseDirectory())
+  core.info(`copying artifact(s) to ${destination}`)
+  await copyFiles(files, destination)
 
   const status = await git.status(workspace)
   if (!status) {
     core.info('nothing to commit')
     return
   }
-
   await git.commit(workspace, 'upload-artifact-wiki')
   await git.push(workspace)
+}
+
+const getBaseDirectory = () => {
+  if (github.context.eventName === 'pull_request') {
+    return `pr-${github.context.payload.pull_request?.number ?? 0}`
+  }
+  return github.context.ref.replace(/^refs\/\w+\//, '')
+}
+
+export const copyFiles = async (files: string[], destinationDirectory: string) => {
+  for (const f of files) {
+    const relativePath = path.relative('.', f)
+    const destination = path.join(destinationDirectory, relativePath)
+    await io.cp(f, destination)
+    core.info(`${f} -> ${destination}`)
+  }
 }

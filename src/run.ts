a913c8db80b5eeac4cf154fe75c97d945a473de4
Hidetake Iwata <int128@gmail.com>
2022-06-14 09:23:15 +0900

    refactor (#7)
    * refactor: test getBaseDirectory()
    
    * refactor: test getWiki()

--- src/run.ts
+++ src/run.ts
@@ -1,11 +1,11 @@
 import * as core from '@actions/core'
 import * as io from '@actions/io'
-import * as github from '@actions/github'
 import * as glob from '@actions/glob'
 import { promises as fs } from 'fs'
 import * as path from 'path'
 import * as os from 'os'
 import * as git from './git'
+import { Context as GitHubContext } from '@actions/github/lib/context'
 
 type Inputs = {
   path: string
@@ -16,42 +16,50 @@ type Outputs = {
   url: string
 }
 
-export const run = async (inputs: Inputs): Promise<Outputs> => {
+type Context = Pick<GitHubContext, 'repo' | 'serverUrl' | 'ref' | 'eventName' | 'payload'>
+
+export const run = async (inputs: Inputs, context: Context): Promise<Outputs> => {
+  const wiki = getWiki(context)
+
+  const workspace = await fs.mkdtemp(path.join(os.tmpdir(), 'upload-artifact-wiki-'))
+  core.info(`cloning ${wiki.repository} into ${workspace}`)
+  await git.clone(workspace, wiki.repository, inputs.token)
+
   const globber = await glob.create(inputs.path, { matchDirectories: false })
   const files = await globber.glob()
-  core.info(`uploading ${files.length} artifact(s)`)
-
-  const wikiRepository = `${github.context.serverUrl}/${github.context.repo.owner}/${github.context.repo.repo}.wiki.git`
-  const workspace = await fs.mkdtemp(path.join(os.tmpdir(), 'upload-artifact-wiki-'))
-  core.info(`cloning ${wikiRepository} into ${workspace}`)
-  await git.clone(workspace, wikiRepository, inputs.token)
-
-  const wikiBaseDirectory = getBaseDirectory()
-  const wikiBaseUrl = `${github.context.serverUrl}/${github.context.repo.owner}/${github.context.repo.repo}/wiki/${wikiBaseDirectory}`
-  core.info(`artifact is published at ${wikiBaseUrl}`)
-
-  const destination = path.join(workspace, wikiBaseDirectory)
-  core.info(`copying artifact(s) to ${destination}`)
+  const destination = path.join(workspace, wiki.baseDirectory)
+  core.info(`copying ${files.length} artifact(s) to ${destination}`)
   await copyFiles(files, destination)
 
   const status = await git.status(workspace)
   if (!status) {
     core.info('nothing to commit')
-    return { url: wikiBaseUrl }
+    core.info(`artifact(s) is published at ${wiki.url}`)
+    return { url: wiki.url }
   }
   await git.commit(workspace, 'upload-artifact-wiki')
   await git.push(workspace)
-  return { url: wikiBaseUrl }
+  core.info(`artifact(s) is published at ${wiki.url}`)
+  return { url: wiki.url }
 }
 
-const getBaseDirectory = () => {
-  if (github.context.eventName === 'pull_request') {
-    return `pr-${github.context.payload.pull_request?.number ?? 0}`
+export const getWiki = (context: Context) => {
+  const baseDirectory = getBaseDirectory(context)
+  return {
+    repository: `${context.serverUrl}/${context.repo.owner}/${context.repo.repo}.wiki.git`,
+    baseDirectory,
+    url: `${context.serverUrl}/${context.repo.owner}/${context.repo.repo}/wiki/${baseDirectory}`,
   }
-  return github.context.ref.replace(/^refs\/\w+\//, '')
 }
 
-export const copyFiles = async (files: string[], destinationDirectory: string) => {
+export const getBaseDirectory = (context: Context) => {
+  if (context.eventName === 'pull_request') {
+    return `pr-${context.payload.pull_request?.number ?? 0}`
+  }
+  return context.ref.replace(/^refs\/\w+\//, '')
+}
+
+const copyFiles = async (files: string[], destinationDirectory: string) => {
   for (const f of files) {
     const relativePath = path.relative('.', f)
     const destination = path.join(destinationDirectory, relativePath)
--- tests/run.test.ts
+++ tests/run.test.ts
@@ -1,3 +1,44 @@
-test('run successfully', async () => {
-  // TODO
+import { getBaseDirectory, getWiki } from '../src/run'
+
+describe('getBaseDirectory returns a directory corresponding to context', () => {
+  test('on pull request', () => {
+    const directory = getBaseDirectory({
+      eventName: 'pull_request',
+      payload: {
+        pull_request: {
+          number: 123,
+        },
+      },
+      repo: { owner: 'owner', repo: 'repo' },
+      ref: 'refs/pulls/123/merge',
+      serverUrl: 'https://github.com',
+    })
+    expect(directory).toBe('pr-123')
+  })
+
+  test('on push', () => {
+    const directory = getBaseDirectory({
+      eventName: 'push',
+      payload: {},
+      repo: { owner: 'owner', repo: 'repo' },
+      ref: 'refs/heads/main',
+      serverUrl: 'https://github.com',
+    })
+    expect(directory).toBe('main')
+  })
+})
+
+test('getWiki', () => {
+  const wiki = getWiki({
+    eventName: 'push',
+    payload: {},
+    repo: { owner: 'owner', repo: 'repo' },
+    ref: 'refs/heads/main',
+    serverUrl: 'https://github.com',
+  })
+  expect(wiki).toStrictEqual({
+    repository: 'https://github.com/owner/repo.wiki.git',
+    baseDirectory: 'main',
+    url: 'https://github.com/owner/repo/wiki/main',
+  })
 })
